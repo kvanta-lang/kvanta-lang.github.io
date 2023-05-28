21af640756a0ac17fc0ad9ce0d2c72fafb601146
IvanRamyk <ivan.r@mate.academy>
2023-05-28 18:12:15 +0300

    accept the field state from quanta.js

--- quanta_code_editor/editor.mjs
+++ quanta_code_editor/editor.mjs
@@ -13,12 +13,16 @@ function run_code() {
   let text = editor.state.doc.toString().trim();
   console.log("text: " + text);
   let canvas = document.getElementById("drawing");
-  if (check_good_text(text)) {
-      canvas.style.backgroundColor = "#00FF00"
-      canvas.innerText = "It is a valid program!";
-  } else {
-      canvas.style.backgroundColor = "#FF0000"
-      canvas.innerText = "It is a not valid program :(";
+  const ctx = canvas.getContext("2d");
+  let canvas_state = check_good_text(text).field;
+  for (var i = 0; i < canvas_state.length; i++) {
+    for (var j = 0; j < canvas_state[i].length; j++) {
+      let r  = canvas_state[i][j] >> 16 & 255;
+      let g  = canvas_state[i][j] >> 8 & 255;
+      let b  = canvas_state[i][j] & 255;
+      ctx.fillStyle = "rgba("+r+","+g+","+b+", 1)";
+      ctx.fillRect( i, j, 1, 1);
+    }
   }
 }
 
@@ -35,7 +39,7 @@ let editor = new EditorView({
     ),
   ],
   doc: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
-  parent: document.getElementById("code")
+  parent: document.getElementById("code"),
 });
 
 
--- www/quanta.js
+++ www/quanta.js
@@ -1,9 +1,40 @@
 import {is_good_text, greet} from '../pkg/game_of_life.js'
 
 export function check_good_text(text) {
+    /**
+    * /**
+    * @typedef {Object} Field
+    * @property {array[600][420]} x - all field pixels
+    * maybe more props in the future
+    * 
+    * 
+    * @param {string} text
+    * @returns {Field}
+    *  
+    * Returns a Field object with state of every pixel in the canvas.
+    */
+
     console.log("check_good_text: " + text);
+    let color = 0xff0000;
     if (text === "function(a, b)") {
-        return true
+        color = 0x00ff00;
     }
-    return false;
+
+    var width = 600;
+    var height = 420;
+    
+    // Create a new 2D array
+    var array = [];
+    
+    // Fill the array with the green color value
+    for (var i = 0; i < height; i++) {
+      var row = [];
+      for (var j = 0; j < width; j++) {
+        row.push(color);
+      }
+      array.push(row);
+    }
+    return {
+        field: array,
+    };
 }
