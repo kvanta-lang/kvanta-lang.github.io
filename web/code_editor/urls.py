f35ce6683b029aeb93443b17c979a75d9c5e7e37
IvanRamyk <ivan.r@mate.academy>
2023-06-02 13:30:26 +0300

    added icon

--- web/code_editor/urls.py
+++ web/code_editor/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic import RedirectView
 
 from code_editor.views import index, program_code, update_title
 
@@ -7,6 +9,8 @@ urlpatterns = [
     path("", index, name="index"),
     path("<uuid:program_code_id>", program_code, name="program_code"),
     path("update_title/<uuid:program_code_id>", update_title, name="update_title"),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
+
 ]
 
 
